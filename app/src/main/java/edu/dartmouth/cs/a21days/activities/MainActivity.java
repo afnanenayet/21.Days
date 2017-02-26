3b1c1093d8a7d7d3637e9fba0433cc217a0eff06
Steven Jiang <Steven@stevens-mbp-2.wireless.dartmouth.edu>
2017-02-26 13:19:23 -0500

    Created Navigation View

--- app/src/main/java/edu/dartmouth/cs/a21days/activities/MainActivity.java
+++ app/src/main/java/edu/dartmouth/cs/a21days/activities/MainActivity.java
@@ -1,15 +1,39 @@
 package edu.dartmouth.cs.a21days.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import edu.dartmouth.cs.a21days.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_analytics:
+
+                            case R.id.action_habits:
+
+                            case R.id.action_settings:
+
+                        }
+                        return true;
+                    }
+                });
     }
+
+
 }
