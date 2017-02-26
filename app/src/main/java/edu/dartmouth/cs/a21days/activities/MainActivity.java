35ba41e02016f52ac1fad21a6df825761048cd3a
Steven Jiang <Steven@stevens-mbp-2.wireless.dartmouth.edu>
2017-02-26 14:11:26 -0500

    Created functional bottom navigation view

--- app/src/main/java/edu/dartmouth/cs/a21days/activities/MainActivity.java
+++ app/src/main/java/edu/dartmouth/cs/a21days/activities/MainActivity.java
@@ -1,13 +1,26 @@
 package edu.dartmouth.cs.a21days.activities;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 import edu.dartmouth.cs.a21days.R;
+import edu.dartmouth.cs.a21days.controllers.ViewPagerAdapter;
+import edu.dartmouth.cs.a21days.views.AnalyticsFragment;
+import edu.dartmouth.cs.a21days.views.HabitsListFragment;
+import edu.dartmouth.cs.a21days.views.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNavigationView;
+    private ArrayList<Fragment> mFragments;
+    private ViewPager mViewPager;
+    private ViewPagerAdapter mViewPageAdapter;
 
 
     @Override
@@ -15,19 +28,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+        mBottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mFragments = new ArrayList<Fragment>();
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(
+        mFragments.add(new AnalyticsFragment());
+        mFragments.add(new HabitsListFragment());
+        mFragments.add(new SettingsFragment());
+
+
+        mViewPageAdapter = new ViewPagerAdapter(getFragmentManager(), mFragments);
+        mViewPager.setAdapter(mViewPageAdapter);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.action_analytics:
-
+                                mViewPager.setCurrentItem(ViewPagerAdapter.ANALYTICS);
+                                break;
                             case R.id.action_habits:
-
+                                mViewPager.setCurrentItem(ViewPagerAdapter.HABITS);
+                                break;
                             case R.id.action_settings:
+                                mViewPager.setCurrentItem(ViewPagerAdapter.SETTINGS);
+                                break;
 
                         }
                         return true;
---
+++ app/src/main/java/edu/dartmouth/cs/a21days/controllers/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package edu.dartmouth.cs.a21days.controllers;
+
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragments;
+
+    // Tab positions and names
+    public static final int ANALYTICS = 0;
+    public static final int HABITS = 1;
+    public static final int SETTINGS = 2;
+
+
+    // View Pager Adapter constructor
+    public ViewPagerAdapter(FragmentManager fragmentManager, ArrayList<Fragment> fragmentList){
+        super(fragmentManager);
+        this.fragments = fragmentList;
+    }
+
+    // Function for getting tab position
+    public Fragment getItem(int pos){
+        return fragments.get(pos);
+    }
+
+    // Function for getting number of tabs
+    public int getCount(){
+        return fragments.size();
+    }
+
+}
+
