d1625fea95864afc3348005ffb4877dbf3b49b5c
nanhu95 <nanhu95@gmail.com>
2017-02-26 19:13:07 -0500

    Test commit

--- app/src/main/java/edu/dartmouth/cs/a21days/views/AnalyticsFragment.java
+++ app/src/main/java/edu/dartmouth/cs/a21days/views/AnalyticsFragment.java
@@ -1,12 +1,20 @@
 package edu.dartmouth.cs.a21days.views;
 
 import android.app.Fragment;
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 import edu.dartmouth.cs.a21days.R;
 
@@ -48,12 +56,52 @@ public class AnalyticsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analytics, container, false);
+        View view = inflater.inflate(R.layout.fragment_analytics, container, false);
+
+        /*************** this section is hard coded stuff to test UI ******************************/
+
+        // testing the bar graph
+        BarChart barChart = (BarChart) view.findViewById(R.id.analytics_chart);
+        // add fake data to chart
+        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+        entries.add(new BarEntry(21, 3)); // num days in streak, num of streaks of these days
+        entries.add(new BarEntry(25, 2));
+        entries.add(new BarEntry(5, 10));
+        entries.add(new BarEntry(10, 5));
+        BarDataSet dataSet = new BarDataSet(entries, "Streaks Summary");
+        dataSet.setColor(Color.RED);
+        // dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        dataSet.setBarBorderWidth(0.9f);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        /*YAxis yAxis = barChart.getAxisLeft();
+        yAxis.setTextColor(Color.RED);
+        yAxis.setAxisMinimum(0f); // start at zero
+        yAxis.setAxisMaximum(12f); // the axis maximum is 12 */
+
+
+
+        BarData barData = new BarData(dataSet);
+        barChart.setData(barData);
+        barChart.setFitBars(true);
+        barChart.invalidate(); // refresh
+
+        // testing the comment
+        TextView encourageComment = (TextView) view.findViewById(R.id.analytics_comment_textview);
+        encourageComment.setText(R.string.analytics_default_comment);
+
+
+        /****************** end hard coded section ************************************************/
+
+        return view;
     }
 }
