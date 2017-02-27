5a4a559b55474022ff930da7406b764266c82a19
nanhu95 <nanhu95@gmail.com>
2017-02-27 12:20:55 -0500

    created skeleton for Habit Detail Activity

---
+++ app/src/main/java/edu/dartmouth/cs/a21days/controllers/HabitDetailActivity.java
@@ -0,0 +1,15 @@
+package edu.dartmouth.cs.a21days.controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.dartmouth.cs.a21days.R;
+
+public class HabitDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_detail);
+    }
+}
