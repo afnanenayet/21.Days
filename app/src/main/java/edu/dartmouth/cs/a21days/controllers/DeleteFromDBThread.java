3bf62701e83bd298b20d450c2d36f97d990a6fc9
Steven Jiang <Steven.jiang.19@dartmouth.edu>
2017-03-06 01:49:04 -0500

    added thread

---
+++ app/src/main/java/edu/dartmouth/cs/a21days/controllers/DeleteFromDBThread.java
@@ -0,0 +1,12 @@
+package edu.dartmouth.cs.a21days.controllers;
+
+/**
+ * Created by Steven on 3/6/17.
+ */
+
+public class DeleteFromDBThread extends Thread {
+
+    DeleteFromDBThread(){
+
+    }
+}
