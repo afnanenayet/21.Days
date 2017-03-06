96f1cb004e5ba7881cf849fc9280af14297a24b1
nanhu95 <nanhu95@gmail.com>
2017-03-05 22:50:28 -0500

    updated comments for files in models folder

--- app/src/main/java/edu/dartmouth/cs/a21days/models/Habit.java
+++ app/src/main/java/edu/dartmouth/cs/a21days/models/Habit.java
@@ -5,77 +5,99 @@ import android.icu.util.Calendar;
 import java.util.ArrayList;
 
 /**
+ * Data model for a habit + relevant properties.
+ *
  * Created by aenayet on 2/24/17.
  */
-
-/**
- * Data model for a habit + relevant properties
- */
 public class Habit {
+    // user ID
     private String id;
+    // habit name
     private String name;
+    // habit location
     private SerialLatLng location;
+    // habit frequency
     private ArrayList<Calendar> frequency;
+    // habit streak
     private int streak;
+    // habit priority
     private int priority;
+    // habit category
     private String category;
 
-    public Habit() {}
+    // constructor
+    public Habit() {
+        // empty
+    }
 
     /************************************** Getters and setters *********************************/
 
+    // get habit category
     public String getCategory() {
         return category;
     }
 
+    // set habit category
     public void setCategory(String category) {
         this.category = category;
     }
 
+    // get habit priority
     public int getPriority() {
         return priority;
     }
 
+    // set habit priority
     public void setPriority(int priority) {
         this.priority = priority;
     }
 
+    // get habit streak
     public int getStreak() {
         return streak;
     }
 
+    // set habit streak
     public void setStreak(int streak) {
         this.streak = streak;
     }
 
+    // get habit frequency
     public ArrayList<Calendar> getFrequency() {
         return frequency;
     }
 
+    // set habit frequency
     public void setFrequency(ArrayList<Calendar> frequency) {
         this.frequency = frequency;
     }
 
+    // get habit location
     public SerialLatLng getLocation() {
         return location;
     }
 
+    // set habit location
     public void setLocation(SerialLatLng serialLatLng) {
         this.location = serialLatLng;
     }
 
+    // set habit name
     public String getName() {
         return name;
     }
 
+    // get habit name
     public void setName(String name) {
         this.name = name;
     }
 
+    // get id
     public String getId() {
         return id;
     }
 
+    // set id
     public void setId(String id) {
         this.id = id;
     }
--- app/src/main/java/edu/dartmouth/cs/a21days/models/SerialLatLng.java
+++ app/src/main/java/edu/dartmouth/cs/a21days/models/SerialLatLng.java
@@ -1,30 +1,36 @@
 package edu.dartmouth.cs.a21days.models;
 
 /**
+ * Serializable version of Location class for Firebase.
+ *
  * Created by aenayet on 3/5/17.
  */
-
-/**
- * Serializable version of Location class for Firebase
- */
 public class SerialLatLng {
-    public SerialLatLng() { }
 
     private Double latitude;
     private Double longitude;
 
+    // constructor
+    public SerialLatLng() {
+        // empty
+    }
+
+    // get latitude
     public Double getLatitude() {
         return latitude;
     }
 
+    // set latitude
     public void setLatitude(Double latitude) {
         this.latitude = latitude;
     }
 
+    // get longitude
     public Double getLongitude() {
         return longitude;
     }
 
+    // set longitude
     public void setLongitude(Double longitude) {
         this.longitude = longitude;
     }
